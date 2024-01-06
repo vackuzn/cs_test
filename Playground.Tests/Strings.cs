8d35851dc377de4ec019696f7b89577c5148662d
vackuzn <vackuzn@gmail.com>
2024-01-07 00:08:00 +0100

    Add string test

--- Playground.Tests/Strings.cs
+++ Playground.Tests/Strings.cs
@@ -3,11 +3,25 @@
 public class Strings
 {
     [Fact]
-    public void Test1()
+    public void StringNewVsCompileTime()
     {
         var s1 = new String("aaa");
         var s2 = "aaa";
 
         Assert.True(s1 == s2);
+    }
+    [Fact]
+    public void EqualsVsRefEquals()
+    {
+        string hello = "hello";
+        string helloWorld = "hello world";
+        string helloWorld2 = "hello world";
+        string helloWorld3 = hello + " world";
+
+        Assert.Equal(helloWorld, helloWorld2);
+        Assert.True(ReferenceEquals(helloWorld, helloWorld2));
+
+        Assert.Equal(helloWorld, helloWorld3);
+        Assert.False(ReferenceEquals(helloWorld, helloWorld3));
     }
 }
